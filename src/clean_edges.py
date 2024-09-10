81ac1762b363b1dc2bc1b3a8ce392eec816f5805
Marcus <mahf@itu.dk>
2024-09-10 10:59:41 +0200

    script for removing all invalid edges

---
+++ src/clean_edges.py
@@ -0,0 +1,44 @@
+# This script only exists due to our poor programming skills earlier in the data collection process.
+# If new edges are collected, 
+# there is no need for cleaning due to more strict string parsing rules with regards to video urls.
+#
+# This script fixes edges such as:
+# https://www.tiktok.com/@woodsy0010/video/7373538097562193169,https://www.tiktok.com/@animegreenly
+# and replaces them with:
+# https://www.tiktok.com/@woodsy0010/video/7373538097562193169,None
+
+if __name__ == '__main__':
+    from sys import argv
+    import os
+    import re
+
+    if len(argv) < 2:
+        raise ValueError('Hashtag must be provided as argument')
+
+    if len(argv) == 3:
+        if argv[2] not in ['duet', 'stitch']:
+            raise ValueError('Duet or stitch must be provided as second argument')
+        duet_or_stitch = argv[2]
+    else:
+        duet_or_stitch = 'stitch'
+
+    hashtag = argv[1]
+
+    with open(f'../data/hashtags/{duet_or_stitch}/edges/{hashtag}_edges.txt', 'r') as f:
+        lines = f.readlines()
+        N = len(lines)
+
+    expression = re.compile(r'\.com/@[\w\d\.]+/video/')
+    
+    for i, line in enumerate(lines):
+        source, target = line.strip().split(',')
+        if bool(re.search(expression, target)) or target == 'None':
+            continue
+
+        print(f'{i}/{N}\t>>> repairing {source},{target}', flush=True)
+        lines[i] = f'{source},None\n'
+
+    os.rename(f'../data/hashtags/{duet_or_stitch}/edges/{hashtag}_edges.txt', f'../data/hashtags/{duet_or_stitch}/edges/dirty/{hashtag}_edges.txt')
+
+    with open(f'../data/hashtags/{duet_or_stitch}/edges/{hashtag}_edges.txt', 'w') as f:
+        f.writelines(lines)
