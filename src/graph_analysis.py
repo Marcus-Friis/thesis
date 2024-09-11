35465b2bffbe94bd28a3ef69cb8e4c60db663ccf
Marcus <mahf@itu.dk>
2024-09-11 14:58:09 +0200

    improved graph analysis with degree centralization ⚙️

--- src/graph_analysis.py
+++ src/graph_analysis.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import re
+from graph_utils import degree_centralization
 
 
 def get_usergraph(edges: pd.DataFrame) -> ig.Graph:
@@ -17,6 +18,97 @@ def get_usergraph(edges: pd.DataFrame) -> ig.Graph:
 
     return G
 
+def output_summary_statistics(G: ig.Graph) -> None:
+    G_un = G.as_undirected()
+    G_simple = G_un.simplify()
+
+    print('Number of vertices:', G.vcount())
+    print('Number of edges:', G.ecount())
+    print('Number of components:', len(G_un.components()))
+    print('Largest component size:', max(G_un.components().sizes()))
+    print('Degree assortativity:', G.assortativity_degree(directed=True))
+    print('Clustering coefficient:', G.transitivity_undirected())
+    print('Diameter:', G.diameter())
+    print('Undirected diameter:', G_un.diameter())
+    print('Reciprocity:', G.reciprocity())
+
+    components = G_simple.components()
+    c_sizes = [len(c) for c in components if len(c) > 2]
+    c_centralizations = [degree_centralization(G_simple.subgraph(c)) for c in components if len(c) > 2]
+
+    lcc_centralization = c_centralizations[np.argmax(c_sizes)]
+    print('Global degree centralization:', degree_centralization(G_simple.as_undirected()))
+    print('Largest component degree centralization:', lcc_centralization)
+    print('Avg local degree centralization:', np.mean(c_centralizations))
+    print('Weighted avg local degree centralization:', np.average(c_centralizations, weights=c_sizes))
+
+
+def plot_degree_distributions(G, label):
+    in_degrees = G.indegree()
+    out_degrees = G.outdegree()
+
+    # get frequency of each degree and normalize to density
+    d_in, v_in = np.unique(in_degrees, return_counts=True)
+    v_in = v_in / v_in.sum()
+    d_out, v_out = np.unique(out_degrees, return_counts=True)
+    v_out = v_out / v_out.sum()
+
+    x_upper_bound = max(np.max(d_in), np.max(d_out))
+    y_upper_bound = max(np.max(v_in), np.max(v_out))
+
+    dot_size = 15
+
+    # plot in-degree
+    fig, ax = plt.subplots(figsize=[6.4*0.75, 4.8*0.75])
+    ax.scatter(d_in, v_in, s=dot_size)
+    ax.set_xlabel('k')
+    ax.set_ylabel('p(k)')
+    # ax.set_title(f'{PLOT_TITLES}in-degree distribution')
+    ax.set_xlim(-10, x_upper_bound + 10)
+    ax.set_ylim(-0.01, y_upper_bound + 0.01)
+    plt.tight_layout()
+    fig.savefig(f'../figures/degree_distributions/{label}-in-degree.svg')
+
+    # plot out-degree
+    fig, ax = plt.subplots(figsize=[6.4*0.75, 4.8*0.75])
+    ax.scatter(d_out, v_out, s=dot_size)
+    ax.set_xlabel('k')
+    ax.set_ylabel('p(k)')
+    # ax.set_title(f'{PLOT_TITLES}out-degree distribution')
+    ax.set_xlim(-10, x_upper_bound + 10)
+    ax.set_ylim(-0.01, y_upper_bound + 0.01)
+    plt.tight_layout()
+    fig.savefig(f'../figures/degree_distributions/{label}-out-degree.svg')
+
+    # plot in-degree log-log
+    fig, ax = plt.subplots(figsize=[6.4*0.75, 4.8*0.75])
+    ax.scatter(d_in, v_in, s=dot_size)
+    ax.set_xlabel('k')
+    ax.set_ylabel('p(k)')
+    # ax.set_title(f'{PLOT_TITLES}in-degree distribution')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlim(0.9, x_upper_bound*2)
+    ax.set_ylim(0.0001, y_upper_bound*2)
+    plt.tight_layout()
+    fig.savefig(f'../figures/degree_distributions/{label}-log-in-degree.svg')
+
+    # plot out-degree log-log
+    fig, ax = plt.subplots(figsize=[6.4*0.75, 4.8*0.75])
+    d_out, v_out = np.unique(out_degrees, return_counts=True)
+    v_out = v_out / v_out.sum()
+    ax.scatter(d_out, v_out, s=dot_size)
+    ax.set_xlabel('k')
+    ax.set_ylabel('p(k)')
+    # ax.set_title(f'{PLOT_TITLES}out-degree distribution')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlim(0.9, x_upper_bound*2)
+    ax.set_ylim(0.0001, y_upper_bound*2)
+    plt.tight_layout()
+    fig.savefig(f'../figures/degree_distributions/{label}-log-out-degree.svg')
+
+
 if __name__ == '__main__':
     plt.style.use('ggplot')
     from sys import argv
@@ -27,7 +119,7 @@ if __name__ == '__main__':
     hashtag = argv[1]
 
     # read edges from file
-    edges = pd.read_csv(f'../data/{hashtag}_edges_repair.txt', header=None)
+    edges = pd.read_csv(f'../data/hashtags/stitch/edges/{hashtag}_edges.txt', header=None)
     edges.columns = ['stitcher_url', 'stitchee_url']
 
     # remove all None edges
@@ -43,75 +135,10 @@ if __name__ == '__main__':
 
     # output summary statistics
     print('Video Graph')
-    print('Number of vertices:', G.vcount())
-    print('Number of edges:', G.ecount())
-    print('Number of components:', len(G.as_undirected().components()))
-    print('Largest component size:', max(G.as_undirected().components().sizes()))
+    output_summary_statistics(G)
 
     # plot degree distribution
-    in_degrees = G.indegree()
-    out_degrees = G.outdegree()
-
-    # get frequency of each degree and normalize to density
-    d_in, v_in = np.unique(in_degrees, return_counts=True)
-    v_in = v_in / v_in.sum()
-    d_out, v_out = np.unique(out_degrees, return_counts=True)
-    v_out = v_out / v_out.sum()
-
-    x_upper_bound = max(np.max(d_in), np.max(d_out))
-    y_upper_bound = max(np.max(v_in), np.max(v_out))
-
-    dot_size = 15
-
-    # plot in-degree
-    fig, ax = plt.subplots(figsize=[6.4*0.75, 4.8*0.75])
-    ax.scatter(d_in, v_in, s=dot_size)
-    ax.set_xlabel('k')
-    ax.set_ylabel('p(k)')
-    # ax.set_title(f'{PLOT_TITLES}in-degree distribution')
-    ax.set_xlim(-10, x_upper_bound + 10)
-    ax.set_ylim(-0.01, y_upper_bound + 0.01)
-    plt.tight_layout()
-    fig.savefig(f'../figures/degree_distributions/{hashtag}-in-degree.svg')
-
-    # plot out-degree
-    fig, ax = plt.subplots(figsize=[6.4*0.75, 4.8*0.75])
-    ax.scatter(d_out, v_out, s=dot_size)
-    ax.set_xlabel('k')
-    ax.set_ylabel('p(k)')
-    # ax.set_title(f'{PLOT_TITLES}out-degree distribution')
-    ax.set_xlim(-10, x_upper_bound + 10)
-    ax.set_ylim(-0.01, y_upper_bound + 0.01)
-    plt.tight_layout()
-    fig.savefig(f'../figures/degree_distributions/{hashtag}-out-degree.svg')
-
-    # plot in-degree log-log
-    fig, ax = plt.subplots(figsize=[6.4*0.75, 4.8*0.75])
-    ax.scatter(d_in, v_in, s=dot_size)
-    ax.set_xlabel('k')
-    ax.set_ylabel('p(k)')
-    # ax.set_title(f'{PLOT_TITLES}in-degree distribution')
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    ax.set_xlim(0.9, x_upper_bound*2)
-    ax.set_ylim(0.0001, y_upper_bound*2)
-    plt.tight_layout()
-    fig.savefig(f'../figures/degree_distributions/{hashtag}-log-in-degree.svg')
-
-    # plot out-degree log-log
-    fig, ax = plt.subplots(figsize=[6.4*0.75, 4.8*0.75])
-    d_out, v_out = np.unique(out_degrees, return_counts=True)
-    v_out = v_out / v_out.sum()
-    ax.scatter(d_out, v_out, s=dot_size)
-    ax.set_xlabel('k')
-    ax.set_ylabel('p(k)')
-    # ax.set_title(f'{PLOT_TITLES}out-degree distribution')
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    ax.set_xlim(0.9, x_upper_bound*2)
-    ax.set_ylim(0.0001, y_upper_bound*2)
-    plt.tight_layout()
-    fig.savefig(f'../figures/degree_distributions/{hashtag}-log-out-degree.svg')
+    plot_degree_distributions(G, hashtag)
 
     # plot graph
     target = f'../figures/video_graphs/{hashtag}-graph.svg'
@@ -126,75 +153,10 @@ if __name__ == '__main__':
 
     # output summary statistics
     print('\nUser Graph')
-    print('Number of vertices:', G.vcount())
-    print('Number of edges:', G.ecount())
-    print('Number of components:', len(G.as_undirected().components()))
-    print('Largest component size:', max(G.as_undirected().components().sizes()))
+    output_summary_statistics(G)
 
     # plot degree distribution
-    in_degrees = G.indegree()
-    out_degrees = G.outdegree()
-
-    # get frequency of each degree and normalize to density
-    d_in, v_in = np.unique(in_degrees, return_counts=True)
-    v_in = v_in / v_in.sum()
-    d_out, v_out = np.unique(out_degrees, return_counts=True)
-    v_out = v_out / v_out.sum()
-
-    x_upper_bound = max(np.max(d_in), np.max(d_out))
-    y_upper_bound = max(np.max(v_in), np.max(v_out))
-
-    dot_size = 15
-
-    # plot in-degree
-    fig, ax = plt.subplots(figsize=[6.4*0.75, 4.8*0.75])
-    ax.scatter(d_in, v_in, s=dot_size)
-    ax.set_xlabel('k')
-    ax.set_ylabel('p(k)')
-    # ax.set_title(f'{PLOT_TITLES}in-degree distribution')
-    ax.set_xlim(-10, x_upper_bound + 10)
-    ax.set_ylim(-0.01, y_upper_bound + 0.01)
-    plt.tight_layout()
-    fig.savefig(f'../figures/degree_distributions/{hashtag}-user-in-degree.svg')
-
-    # plot out-degree
-    fig, ax = plt.subplots(figsize=[6.4*0.75, 4.8*0.75])
-    ax.scatter(d_out, v_out, s=dot_size)
-    ax.set_xlabel('k')
-    ax.set_ylabel('p(k)')
-    # ax.set_title(f'{PLOT_TITLES}out-degree distribution')
-    ax.set_xlim(-10, x_upper_bound + 10)
-    ax.set_ylim(-0.01, y_upper_bound + 0.01)
-    plt.tight_layout()
-    fig.savefig(f'../figures/degree_distributions/{hashtag}-user-out-degree.svg')
-
-    # plot in-degree log-log
-    fig, ax = plt.subplots(figsize=[6.4*0.75, 4.8*0.75])
-    ax.scatter(d_in, v_in, s=dot_size)
-    ax.set_xlabel('k')
-    ax.set_ylabel('p(k)')
-    # ax.set_title(f'{PLOT_TITLES}in-degree distribution')
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    ax.set_xlim(0.9, x_upper_bound*2)
-    ax.set_ylim(0.0001, y_upper_bound*2)
-    plt.tight_layout()
-    fig.savefig(f'../figures/degree_distributions/{hashtag}-user-log-in-degree.svg')
-
-    # plot out-degree log-log
-    fig, ax = plt.subplots(figsize=[6.4*0.75, 4.8*0.75])
-    d_out, v_out = np.unique(out_degrees, return_counts=True)
-    v_out = v_out / v_out.sum()
-    ax.scatter(d_out, v_out, s=dot_size)
-    ax.set_xlabel('k')
-    ax.set_ylabel('p(k)')
-    # ax.set_title(f'{PLOT_TITLES}out-degree distribution')
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    ax.set_xlim(0.9, x_upper_bound*2)
-    ax.set_ylim(0.0001, y_upper_bound*2)
-    plt.tight_layout()
-    fig.savefig(f'../figures/degree_distributions/{hashtag}-user-log-out-degree.svg')
+    plot_degree_distributions(G, hashtag+'-user')
 
     # plot graph
     target = f'../figures/user_graphs/{hashtag}-user-graph.svg'
--- src/graph_utils.py
+++ src/graph_utils.py
@@ -1,22 +1,31 @@
 import igraph as ig
 import numpy as np
 
-def centralization(G: ig.Graph) -> float:
+def degree_centralization(G: ig.Graph) -> float:
     if G.is_directed():
         raise ValueError('Centralization is only defined for undirected graphs')
+
+    if G.vcount() < 3:
+        raise ValueError('Centralization is only defined for graphs with at least 3 vertices')
+
+    if not G.is_simple():
+        raise ValueError('Centralization is only defined for simple graphs')
+
+    if G.ecount() == 0:
+        raise ValueError('Centralization is only defined for graphs with at least 1 edge')
     
     num_nodes = len(G.vs)
-    G2 = ig.Graph.Star(num_nodes, mode = 'undirected')
+    star = ig.Graph.Star(num_nodes, mode = 'undirected')
 
-    degrees_nominator = np.array(G.degree())
-    degrees_denominator = np.array(G2.degree())
+    degrees = np.array(G.degree())
+    degrees_star = np.array(star.degree())
 
-    max_centrality_nominator = np.max(degrees_nominator)
-    max_centrality_denominator = np.max(degrees_denominator)
+    max_degree = np.max(degrees)
+    max_degree_star = np.max(degrees_star)
 
-    centrality_nominator = max_centrality_nominator - degrees_nominator
-    centrality_denominator = np.where(degrees_denominator > 0, max_centrality_denominator - degrees_denominator , 0)
+    centrality = max_degree - degrees
+    centrality_star = np.where(degrees_star > 0, max_degree_star - degrees_star , 0)
 
-    network_centralization = np.sum(centrality_nominator) / np.sum(centrality_denominator)
+    degree_centralization = np.sum(centrality) / np.sum(centrality_star)
     
-    return network_centralization
+    return degree_centralization
