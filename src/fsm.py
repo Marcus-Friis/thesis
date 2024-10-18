4cab05f1e07a94416599aa19999968dd9ee3e77e
Marcus Friis <marcus.friis99@gmail.com>
2024-10-18 13:55:49 +0200

    improved parsing of gspan and nel files

--- src/fsm.py
+++ src/fsm.py
@@ -1,3 +1,4 @@
+from graph_utils import load_edges, get_user_graph
 import igraph as ig
 import os
 
@@ -17,33 +18,15 @@ def igraph_to_gspan(graphs: list) -> str:
         for e in g.es:
             output += f'e {e.source} {e.target} 0\n'
     return output
-    
-def gspan_to_igraph(gspan_content: str) -> list:
-    graphs = []
-    current_graph_edges = []
-    in_graph = False
-    for line in gspan_content.strip().splitlines():
-        if line.startswith('t'):
-            if in_graph:
-                # Create a graph for the previous block
-                g = ig.Graph.TupleList(current_graph_edges, directed=False)
-                graphs.append(g)
-                current_graph_edges = []  # Reset for the next graph
-            in_graph = True
-        elif line.startswith('v'):
-            # Vertex information can be ignored for igraph
-            continue
-        elif line.startswith('e'):
-            # Extract edge information
-            _, source, target, _ = line.split()
-            current_graph_edges.append((int(source), int(target)))
-    if current_graph_edges:
-        # Add the final graph
-        g = ig.Graph.TupleList(current_graph_edges, directed=False)
-        graphs.append(g)
-    return graphs
 
 def igraph_to_nel(graphs: list) -> str:
+    """
+    v 1
+    v 2
+    e 1 2
+    g 1
+    x 0
+    """
     output = ''
     for i, g in enumerate(graphs):
         for v in g.vs:
@@ -55,32 +38,35 @@ def igraph_to_nel(graphs: list) -> str:
     output = output.strip() + '\n'
     return output
 
-def nel_to_igraph(nel_content: str) -> list:
-    """
-    v 1
-    v 2
-    e 1 2
-    g 1
-    x 0
-    """
+def gspan_to_igraph(gspan_content: str) -> list:
     graphs = []
-    current_graph_edges = []
-    in_graph = False
-    for line in nel_content.strip().splitlines():
+    edges = []
+    lines = gspan_content.strip().split('\n')
+    for line in lines:
         if line.startswith('e'):
-            _, source, target = line.split()
-            current_graph_edges.append((int(source) - 1, int(target) - 1))
-        elif line.startswith('g'):
-            if in_graph and current_graph_edges:
-                g = ig.Graph.TupleList(current_graph_edges, directed=True)
-                graphs.append(g)
-                current_graph_edges = []  # Reset for the next graph
-            in_graph = True
-    if current_graph_edges:
-        # Add the final graph as directed
-        g = ig.Graph.TupleList(current_graph_edges, directed=True)
-        graphs.append(g)
+            u, v = int(line.split(' ')[1]), int(line.split(' ')[2])
+            edges.append((u, v))
+        elif line.startswith('t') and line != 't # 0':
+            g = ig.Graph.TupleList(edges, directed=False)
+            graphs.append(g)
+            edges = []
+    g = ig.Graph.TupleList(edges, directed=False)
+    graphs.append(g)
     return graphs
+
+def nel_to_igraph(nel_content: str) -> list:
+    graphs = []
+    edges = []
+    lines = nel_content.strip().split('\n')
+    for line in lines:
+        if line.startswith('e'):
+            u, v = int(line.split(' ')[1]), int(line.split(' ')[2])
+            edges.append((u, v))
+        elif line.startswith('g'):
+            g = ig.Graph.TupleList(edges, directed=True)
+            graphs.append(g)
+            edges = []
+    return graphs            
 
 
 if __name__ == '__main__':
@@ -93,25 +79,14 @@ if __name__ == '__main__':
     for i, edge_file in enumerate(edge_files):
         # read edge file
         edge_file_path = os.path.join(data_path, edge_file)
-        with open(edge_file_path, 'r') as f:
-            edges = f.readlines()
+        edges = load_edges(edge_file_path)
+        g = get_user_graph(edges)
 
-        # construct user graph edgelist
-        users = []
-        for edge in edges:
-            u, v = edge.strip().split(',')
-            if 'None' in v:
-                continue
-            u_user = u.split('/')[-3]
-            v_user = v.split('/')[-3]
-            if (u_user, v_user) not in users:
-                users.append((u_user, v_user))
-
-        # construct lcc graph
-        g = ig.Graph.TupleList(users, directed=True).simplify()
+        # get lcc
         lcc = g.as_undirected().components().giant()
-        largest_components_nodes = lcc.vs.indices
-        g_lcc = g.subgraph(largest_components_nodes)
+        g_lcc = g.subgraph(lcc.vs.indices)
+
+        # append graphs
         graphs.append(g)
         lcc_graphs.append(g_lcc)
 
