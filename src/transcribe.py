3fc3209d399b717c22d1d1c8c23fc4f439c63825
Madshogenhaug <madshogenhaug3@gmail.com>
2024-11-12 23:37:13 +0100

    updated script, close to done

--- src/transcribe.py
+++ src/transcribe.py
@@ -16,11 +16,14 @@ from mediapipe.tasks.python.components import containers
 from mediapipe.tasks.python import audio
 from pathlib import Path
 
+from pydub import AudioSegment
+import io
+
 from sys import argv
 import urllib.request
 
-def classify_language(video_path, model_name='base'):
-    model = whisper.load_model(model_name)  
+def classify_language(video_path, model):
+ 
 
     audio_data = whisper.load_audio(video_path)
     audio_data = whisper.pad_or_trim(audio_data)
@@ -30,56 +33,61 @@ def classify_language(video_path, model_name='base'):
 
     return max(probs, key=probs.get)
 
-def extract_audio(video_path, audio_path):
+def extract_audio(video_path):
+    # Load the video and extract audio as a temporary file
     video = VideoFileClip(video_path)
-    audio = video.audio
-    audio.write_audiofile(audio_path)
-    audio.close()
+    temp_audio_path = "temp_audio.wav"  # Temporary path to save audio initially
+    video.audio.write_audiofile(temp_audio_path)
+    
+    # Use pydub to load audio and convert to in-memory bytes buffer
+    audio = AudioSegment.from_file(temp_audio_path, format="wav")
+    audio_buffer = io.BytesIO()
+    audio.export(audio_buffer, format="wav")
+    
+    # Close resources and remove temporary file
     video.close()
-    return audio_path
+    audio_buffer.seek(0)  # Reset buffer position to beginning
+    
+    return audio_buffer
 
-def detect_audio_type(audio_path):
-    if not audio_path.endswith('.wav'):
-        raise ValueError("The audio file must be a .wav file")
+def detect_audio_type(audio_path, classifier):
 
-    base_options = python.BaseOptions(model_asset_path='classifier.tflite')
-    options = audio.AudioClassifierOptions(base_options=base_options, max_results=4)
+
     category_counts = defaultdict(int)
     timestamped_classifications = []
 
-    with audio.AudioClassifier.create_from_options(options) as classifier:
-        sample_rate, wav_data = wavfile.read(audio_path) 
-        audio_clip = containers.AudioData.create_from_array(
-            wav_data.astype(float) / np.iinfo(np.int16).max, sample_rate
-        )
-        classification_result_list = classifier.classify(audio_clip)
-        audio_duration_ms = (len(wav_data) / sample_rate) * 1000
-        interval_ms = 975
-        timestamps = np.arange(0, audio_duration_ms, interval_ms).astype(int)
+    sample_rate, wav_data = wavfile.read(audio_path)
+    audio_clip = containers.AudioData.create_from_array(
+        wav_data.astype(float) / np.iinfo(np.int16).max, sample_rate
+    )
+    classification_result_list = classifier.classify(audio_clip)
+    audio_duration_ms = (len(wav_data) / sample_rate) * 1000
+    interval_ms = 975
+    timestamps = np.arange(0, audio_duration_ms, interval_ms).astype(int)
 
-        for idx, timestamp in enumerate(timestamps):
-            if idx < len(classification_result_list):
-                classification_result = classification_result_list[idx]
-                top_category = classification_result.classifications[0].categories[0]
+    for idx, timestamp in enumerate(timestamps):
+        if idx < len(classification_result_list):
+            classification_result = classification_result_list[idx]
+            top_category = classification_result.classifications[0].categories[0]
 
-                category_counts[top_category.category_name] += 1
+            category_counts[top_category.category_name] += 1
 
-                # Convert `timestamp` and `score` to standard Python types for JSON compatibility
-                timestamped_classifications.append({
-                    "timestamp": int(timestamp),  # Convert to native Python int
-                    "category_name": top_category.category_name,
-                    "score": float(top_category.score)  # Convert to native Python float
-                })
+            # Convert timestamp and score to standard Python types for JSON compatibility
+            timestamped_classifications.append({
+                "timestamp": int(timestamp),  # Convert to native Python int
+                "category_name": top_category.category_name,
+                "score": float(top_category.score)  # Convert to native Python float
+            })
 
-                print(f'Timestamp {timestamp}: {top_category.category_name} ({top_category.score:.2f})')
+            print(f'Timestamp {timestamp}: {top_category.category_name} ({top_category.score:.2f})')
 
-        final_classification = max(category_counts, key=category_counts.get)
-        print(f'The video {audio_path} is primarily {final_classification}')
+    final_classification = max(category_counts, key=category_counts.get)
+    print(f'The video is primarily {final_classification}')
 
     return timestamped_classifications, final_classification
 
-def transcribe_video(video_path, model_name='base'):
-    model = whisper.load_model(model_name)  
+
+def transcribe_video(video_path, model):
     audio_data = whisper.load_audio(video_path)
     audio_data = whisper.pad_or_trim(audio_data)
 
@@ -95,15 +103,12 @@ def transcribe_video(video_path, model_name='base'):
     return result.text
 
 def extract_id(filename):
-    match = re.search(r'(\d{6,})(?:-Scene-(\d{3}))?\.mp4$', filename)
+    match = re.search(r'_video_(\d+)-Scene-(\d{3})\.mp4$', filename)
     if match:
         video_id_str = match.group(1)
         scene_number_str = match.group(2)
         video_id = np.int64(video_id_str)
-        if scene_number_str:
-            scene_number = int(scene_number_str.lstrip('0'))  # Remove leading zeros
-        else:
-            scene_number = 'NA'
+        scene_number = int(scene_number_str.lstrip('0'))  # Remove leading zeros
         return video_id, scene_number
     return None, 'NA'
 
@@ -112,6 +117,7 @@ def main():
     if len(argv) < 2:
         raise ValueError('At least one hashtag must be provided as argument')
     hashtag = [arg.lower() for arg in argv[1:]][0]
+    print(f"Processing hashtag: {hashtag}")
 
     # Download model for audio classification
     url = "https://storage.googleapis.com/mediapipe-models/audio_classifier/yamnet/float32/1/yamnet.tflite"
@@ -127,43 +133,78 @@ def main():
     # Set the base path for the data
     base_path = Path(f'D:/Thesis/data/{hashtag}/split')
     n = len(list(base_path.glob("*.mp4")))
-    n = 1
     print(f"Total videos to process: {n}")
 
-    # Open the output file once before the loop
-    with open('output.json', 'w') as json_file:
-        # Iterate over all .mp4 files in the directory
-        for i, video_file in enumerate(base_path.glob("*.mp4"), 1):
-            video_path = str(video_file)
+    # Load models for language classification and audio transcription
+    whisper_model = whisper.load_model('base')
+    base_options = python.BaseOptions(model_asset_path='classifier.tflite')
+    options = audio.AudioClassifierOptions(base_options=base_options, max_results=4)
+    classifier = audio.AudioClassifier.create_from_options(options)
+    c = 0 # for testing
 
-            video_id, scene_number = extract_id(video_path)
-            if video_id is None:
-                print(f"Skipping file {video_path}: Unable to extract video ID.")
-                continue
 
-            print(f"Processing video {i}/{n}: {video_path}")
+    if os.path.exists("output.json"):
+        with open("output.json", "r") as file:
+            # Load each (video_id, scene) as a tuple and convert video_id to np.int64
+            processed_videos = [
+                (np.int64(json.loads(line)["video_id"]), json.loads(line)["scene"]) 
+                for line in file
+        ]
 
-            language = classify_language(video_path)
-            is_english = language == "en"
-            audio_path = extract_audio(video_path, 'audio.wav')
-            transcription = transcribe_video(video_path)
-            timestamped_classifications, audio_type = detect_audio_type(audio_path)
 
-            # Collect the output data
-            output_data = {
-                "video_id": int(video_id),
-                "scene": scene_number,
-                "is_english": is_english,
-                "is_speech": audio_type == "Speech",
-                "transcription": transcription,
-                "timestamped_classifications": timestamped_classifications
-            }
+    try:
+        with open('output.json', 'a') as json_file:
+            for i, video_file in enumerate(base_path.glob("*.mp4"), 1):
+                video_path = str(video_file)
 
-            # Write each JSON object on a new line
-            json_file.write(json.dumps(output_data) + '\n')
-            print(f"Processed video {i}/{n}")
+                # Extract both video_id and scene_number
+                video_id, scene_number = extract_id(video_path)
 
-            break
+                # Create the (video_id, scene_number) tuple
+                video_key = (video_id, scene_number)
+
+                # Check if this (video_id, scene_number) pair is already processed
+                if video_key in processed_videos:
+                    print(f"Skipping file {video_path}: Video ID and scene already processed.")
+                    continue
+
+                print(f"Processing video {i}/{n}: {video_path}")
+
+
+                language = classify_language(video_path, model=whisper_model)
+                is_english = language == "en"
+                audio_path = extract_audio(video_path)
+                timestamped_classifications, audio_type = detect_audio_type(audio_path, classifier)
+                if audio_type == "Speech":
+                    transcription = transcribe_video(video_path, model=whisper_model)
+                
+
+
+                output_data = {
+                    "video_id": int(video_id),
+                    "username": video_path.split("\\")[-1].split("_")[0][2:] if video_path.startswith("D:\\Thesis\\data\\anime\\split\\@") else None,
+                    "scene": scene_number,
+                    "language": language,
+                    "is_english": is_english,
+                    "audio_type": audio_type,
+                    "is_speech": audio_type == "Speech",
+                    "transcription": transcription if audio_type == "Speech" else None,
+                    "timestamped_classifications": timestamped_classifications
+                }
+
+
+                # Write each JSON object on a new line
+                json_file.write(json.dumps(output_data) + '\n')
+                print(f"Processed video {i}/{n}")
+
+                # c += 1 # for testing
+                # if c == 4: # for testing
+                #     break   # for testing
+
+    finally:
+        classifier.close()
+        os.remove("temp_audio.wav")
+        print("Classifier closed & temporary audio file removed.")
 
     print("Results saved to output.json")
 
